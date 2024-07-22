b663bef4b8b03c6adbc43969394a0336ac3bf0e6
Jredzx <juanjoserh.754@gmail.com>
2024-07-22 14:29:12 -0500

    Práctica V4

--- 3-dinamic-dom/app.js
+++ 3-dinamic-dom/app.js
@@ -1,16 +1,41 @@
 const listaColores = document.querySelector('#listaColores');
 const btnCrear = document.querySelector('#btnCrear');
 const main = document.querySelector('#main');
+const btnBorrar = document.querySelector('#btnBorrar');
+let n = 1;
+
 
 function crear (){
+
     const ball = document.createElement('div');
-    ball.innerText = "pelotica";
+    const equis = document.createElement('button');
+
+    equis.innerText = 'x';
+    equis.classList.add('equis');
+
     ball.classList.add('pelotica');
     ball.style.backgroundColor = listaColores.value
+    ball.innerText = n++;
+    /*     ball.innerHTML = equis; */
+    ball.appendChild(equis);
+
     main.appendChild(ball);
-
-
-    console.log(ball)
 }
 
-btnCrear.addEventListener('click', crear)
+    function borrarPantalla (){
+        main.innerHTML = '';
+        n=1;
+    }
+    function borrarElemento (e){ 
+        if(e.target.innerText == 'x'){
+            main.removeChild(e.target.parentElement);
+        }
+    }
+    function completarTarea (e){ 
+        e.target.style.textDecoration = 'line-through';
+    }
+    
+    btnCrear.addEventListener('click',crear)
+    btnBorrar.addEventListener('click',borrarPantalla)
+    main.addEventListener('click',borrarElemento)
+    main.addEventListener('click',completarTarea)
